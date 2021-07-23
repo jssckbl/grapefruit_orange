8846d3ad6ea35e69843126f444ae7c9c04204d5f
jssckbl <jessicawoudsma@gmail.com>
2021-07-22 23:22:18 -0500

    update text for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,32 +2,54 @@ import React from "react"
 
 const AboutPage = () => (
   <>
-    <h1>What is Bonneville?</h1>
+    <h1>Who is Jessica?</h1>
     <p>
-      Vestibulum rhoncus quam sit amet purus convallis vestibulum. Pellentesque
-      faucibus, augue id auctor venenatis, tortor eros dapibus ante, et pulvinar
-      libero justo sit amet magna. Suspendisse ac dui lorem. Donec elementum
-      euismod bibendum. Sed eget accumsan dolor.
+      Being a career-changer, Jessica has worn many hats. Not in the literal
+      sense. She is very excited when she finds something she likes and is
+      effective, meaning she has had the same winter hat since 2005.
     </p>
     <p>
-      Mauris luctus metus sed mauris ultrices, vel ullamcorper purus faucibus.
-      Vestibulum consectetur eros elit, nec molestie felis cursus eleifend.
-      Morbi at nisi sollicitudin, vestibulum nibh eget, porttitor diam.
-      Phasellus vestibulum quis lacus nec mollis. Mauris eget sollicitudin enim.
-      Cras fringilla aliquam tellus. Integer eget purus porttitor, pellentesque
-      magna vitae, suscipit eros.
-    </p>
-    <h2>Mauris nec pulvinar est</h2>
-    <p>
-      Quisque et aliquam magna. Pellentesque suscipit odio condimentum, faucibus
-      ante et, fringilla ipsum. In scelerisque laoreet lorem, non eleifend ex
-      eleifend vitae. Phasellus fermentum placerat turpis, eget aliquet turpis
-      auctor in.
+      But back to job hats. There are the radio station gigs, both on-air and
+      producing PSAs and commercials. Playing cello with guitar friends who were
+      in awe of the cello. Interning at a record company in Nashville, TN. Did
+      you know that interning can also show a person what they do not want to do
+      as a job? It's true.
     </p>
     <p>
-      Nulla facilisi. Fusce id elit ac eros fringilla semper. Suspendisse sed
-      placerat metus, sit amet rhoncus lectus. Morbi ultrices auctor mauris, nec
-      volutpat lacus tristique interdum.
+      There are the various barista experiences, both corporate coffee and small
+      tourist town with a line out the door for hours types of experiences.
+      Temping at an insurance company, which led to a Mountain Dew addiction
+      because 40 hours a week of inputting payments into a DOS system needs
+      excitement somewhere, somehow.
+    </p>
+    <p>But wait. There's more.</p>
+    <p>
+      Working for the University of Tennessee. Cleaning houses for high-end
+      clients. Photography assistant. Slinging supplements in a small-town
+      health store. Working on multiple farms. Helping build a food hub that
+      supported local farmers, as well as area schools and restaurants. Washing
+      dishes and plating food in a cramped but fun kitchen. As well as a
+      fantastic few years at a larger food co-op.
+    </p>
+    <p>So why software development, and why now?</p>
+    <h2>Now Presenting...</h2>
+    <p>
+      I love learning the various perspectives of not just a job, but a system.
+      Knowing your audience is guaranteed to make a person more effective at
+      whatever work they are doing.
+    </p>
+    <p>
+      Websites are everywhere. Every day, it is becoming more commonplace that a
+      person or company needs a well-functioning website, whether it is
+      customer-facing, or internal.
+    </p>
+    <p>
+      I will dare say that I am not necessarily passionate about code,
+      specifically. But I am absolutely passionate about learning, growing, and
+      making things easier for others. Code is a vehicle to do that for millions
+      of people. All of those zeros and ones, somehow, allow you to read these
+      words right now on a laptop? iPad? Tablet? Phone? TV screen? It is
+      absolutely incredible.
     </p>
     <h2>Quisque et aliquam magna</h2>
     <p>
